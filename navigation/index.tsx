9e57dd25e6d0ce174e207848eeb2e975b1810fb0
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-02-06 21:45:32 +0100

    header and navigations tabs

---
+++ navigation/index.tsx
@@ -0,0 +1,66 @@
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { ColorSchemeName } from 'react-native';
+
+import NotFoundScreen from '../screens/NotFoundScreen';
+import { RootStackParamList } from '../types';
+import MainTabNavigator from './MainTabNavigator';
+import LinkingConfiguration from './LinkingConfiguration';
+import Colors from '../constants/Colors'
+import { View } from '../components/Themed';
+
+//icons
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+  return (
+    <NavigationContainer
+      linking={LinkingConfiguration}
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+// A root stack navigator is often used for displaying modals on top of all other content
+// Read more here: https://reactnavigation.org/docs/modal
+const Stack = createStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator 
+    screenOptions={{ headerShown: true,
+    headerStyle:{
+    backgroundColor:Colors.light.tint,
+    shadowOpacity:0,
+    elevation:0,
+    borderBottomWidth:0
+  },
+    headerTintColor:Colors.light.background,
+    headerTitleAlign:'left',
+    headerTitleStyle:{fontWeight:'bold'},
+    }}>
+      <Stack.Screen name="Root"
+       component={MainTabNavigator}
+       options={{
+         title:"BarbraStreisand-Chat",
+         headerRight:()=>(
+           <View style={{flexDirection:'row',
+            width:60, 
+            justifyContent:'space-between',
+            marginRight:10,
+            backgroundColor:'transparent'
+            }}>
+             <Ionicons name="md-search" size={24} color={'white'}/>
+             <MaterialCommunityIcons name="dots-vertical" size={24} color={'white'}/>
+           </View>
+         )
+        
+        }}
+       />
+      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+    </Stack.Navigator>
+  );
+}
